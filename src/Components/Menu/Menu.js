2e335bc8ebc734f33cf782a3c03ab756bbf005ca
Wesley_ <47366440+wesleysousaa@users.noreply.github.com>
2023-04-05 21:37:07 -0300

    feat: menu

---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "bootswatch/dist/simplex/bootstrap.min.css";
+import { Link } from 'react-router-dom';
+
+const Menu = () => {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style={{width: "100%"}}>
+      <div class="container-fluid">
+        <Link class="navbar-brand">
+          <h1>Tool <span style={{color: "#1b1b24", borderBottom: "solid 4px #1b1b24"}}>Box</span></h1>
+        </Link>
+        <div style={{ display: 'flex', justifyContent: 'center', marginRight: '1em' }}>
+          <div class="collapse navbar-collapse" id="navbarColor03" >
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item">
+                <Link class="nav-link" to={"/"}>Home</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to={"/tool"}>Ferramenta</Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" to={"/box"}>Caixa</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Menu
